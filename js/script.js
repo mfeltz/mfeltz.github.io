9703bc6bacb09a5cb7af5b664eb0482f407afb8e
Margaret <margaret.feltz@tufts.edu>
2015-02-27 17:02:25 -0500

    added more

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	}
 
 	//Scroll Down to sections
-	$('.logo_nav .link_home').click(function(){
+	$('.icon_nav .link_home').click(function(){
 		$("html, body").animate({scrollTop: 0}, 600);
 		return false;
 	});
@@ -21,27 +21,41 @@ $(document).ready(function(){
 		return false;
 	});
 	$('.nav .link_about').click(function(){
-		$("html, body").animate({scrollTop: 650}, 600);
+		$("html, body").animate({scrollTop: 525}, 600);
 		return false;
 	});
 	$('.nav .link_work').click(function(){
-		$("html, body").animate({scrollTop: 1150}, 600);
+		$("html, body").animate({scrollTop: 1085}, 600);
 		return false;
 	});
 	$('.nav .link_projects').click(function(){
-		$("html, body").animate({scrollTop: 1650}, 600);
+		$("html, body").animate({scrollTop: 1645}, 600);
 		return false;
 	});
 	$('.nav .link_contact').click(function(){
-		$("html, body").animate({scrollTop: 2150}, 600);
+		$("html, body").animate({scrollTop: 2205}, 600);
 		return false;
 	}); 
 
+	//GENERAL FOR JOBS
+	$(".job").on("mouseenter", function(){
+		$(".default").hide();
+	}).on("mouseleave", function() {
+		$(".default").show();
+	});
 
-	$('.IBM_pic').click(function() {
-		
-	})
-
+	//IBM
+	$(".IBM").on("mouseenter", function(){
+		$(".IBM_info").show();
+	}).on("mouseleave", function() {
+		$(".IBM_info").hide();
+	});
+	//TSR
+	$(".TSR").on("mouseenter", function(){
+		$(".TSR_info").show();
+	}).on("mouseleave", function() {
+		$(".TSR_info").hide();
+	});
 
 });
 
