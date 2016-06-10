2864d98ffa4eea5421c303e9dc3634d1ca48d74a
mfeltz <margaret.feltz@tufts.edu>
2016-06-09 22:04:20 -0400

    new website

---
+++ archive/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+	window.onscroll = function(e){
+		var doc = document.documentElement, body = document.body;
+		var top = (doc && doc.scrollTop || body && body.scrollTop || 0);
+
+		if(top > 140) {
+			$('.scroll').fadeIn('slow');
+		} else {
+			$('.scroll').fadeOut('fast');
+		}
+	}
+
+	//Scroll Down to sections
+	$('.icon_nav .link_home').click(function(){
+		$("html, body").animate({scrollTop: 0}, 600);
+		return false;
+	});
+	$('.nav .link_home').click(function(){
+		$("html, body").animate({scrollTop: 0}, 600);
+		return false;
+	});
+	$('.nav .link_about').click(function(){
+		$("html, body").animate({scrollTop: 525}, 600);
+		return false;
+	});
+	$('.nav .link_work').click(function(){
+		$("html, body").animate({scrollTop: 1085}, 600);
+		return false;
+	});
+	$('.nav .link_projects').click(function(){
+		$("html, body").animate({scrollTop: 1645}, 600);
+		return false;
+	});
+	$('.nav .link_contact').click(function(){
+		$("html, body").animate({scrollTop: 2205}, 600);
+		return false;
+	}); 
+
+
+
+	//GENERAL FOR JOBS
+	$(".job").on("mouseenter", function(){
+		$(".default").hide();
+	}).on("mouseleave", function() {
+		$(".default").show();
+	});
+
+	//IBM
+	$(".IBM").on("mouseenter", function(){
+		$(".IBM_info").show();
+	}).on("mouseleave", function() {
+		$(".IBM_info").hide();
+	});
+	//TSR
+	$(".TSR").on("mouseenter", function(){
+		$(".TSR_info").show();
+	}).on("mouseleave", function() {
+		$(".TSR_info").hide();
+	});
+
+});
+
--- js/script.js
+++ js/script.js
@@ -1,63 +1,41 @@
-$(document).ready(function(){
+$( document ).ready(function() {
 
-	window.onscroll = function(e){
-		var doc = document.documentElement, body = document.body;
-		var top = (doc && doc.scrollTop || body && body.scrollTop || 0);
+	//NAV SCROLLING
+	var attachNavScroll = function(link, top) {
+		$(link).click(function() {
+			$('html, body').animate({scrollTop: top}, 600);
+			return false;
+		})
+	};
 
-		if(top > 140) {
-			$('.scroll').fadeIn('slow');
-		} else {
-			$('.scroll').fadeOut('fast');
-		}
-	}
+	var section_1 = 0;
+	var section_2 = 750;
+	var section_3 = 1370;
+	var section_4 = 1920;
+	var section_5 = 2470;
+	var section_6 = 2800;
 
-	//Scroll Down to sections
-	$('.icon_nav .link_home').click(function(){
-		$("html, body").animate({scrollTop: 0}, 600);
-		return false;
-	});
-	$('.nav .link_home').click(function(){
-		$("html, body").animate({scrollTop: 0}, 600);
-		return false;
-	});
-	$('.nav .link_about').click(function(){
-		$("html, body").animate({scrollTop: 525}, 600);
-		return false;
-	});
-	$('.nav .link_work').click(function(){
-		$("html, body").animate({scrollTop: 1085}, 600);
-		return false;
-	});
-	$('.nav .link_projects').click(function(){
-		$("html, body").animate({scrollTop: 1645}, 600);
-		return false;
-	});
-	$('.nav .link_contact').click(function(){
-		$("html, body").animate({scrollTop: 2205}, 600);
-		return false;
-	}); 
+	attachNavScroll('#home-nav', section_1);
+	attachNavScroll('#about-nav', section_2);
+	attachNavScroll('#work-nav', section_3);
+	attachNavScroll('#projects-nav', section_4);
+	attachNavScroll('#resumes-nav', section_5);
+	attachNavScroll('#contact-nav', section_6);
 
+	//MODAL
+	var initializeModal = function() {
+		var showOverlayClass = 'showOverlay';
 
+		$('.project-bubble, .work-bubble').click(function() {
+			$('#overlay').addClass(showOverlayClass);
+		});
 
-	//GENERAL FOR JOBS
-	$(".job").on("mouseenter", function(){
-		$(".default").hide();
-	}).on("mouseleave", function() {
-		$(".default").show();
-	});
-
-	//IBM
-	$(".IBM").on("mouseenter", function(){
-		$(".IBM_info").show();
-	}).on("mouseleave", function() {
-		$(".IBM_info").hide();
-	});
-	//TSR
-	$(".TSR").on("mouseenter", function(){
-		$(".TSR_info").show();
-	}).on("mouseleave", function() {
-		$(".TSR_info").hide();
-	});
+		$('#overlay').click(function(evt){
+            if(evt.target.className === showOverlayClass) {
+                $('#overlay').removeClass(showOverlayClass);
+            }
+        });
+	};
+	initializeModal();
 
 });
-
